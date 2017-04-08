8672408f623bf6fff2bb1f9b7e37a87aba1c8775
ylyinzju <736498802@qq.com>
2017-04-08 13:49:21 +0800

    Initial Commit

---
+++ HBQ/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HBQ
+//
+//  Created by 尹艳玲 on 2017/4/8.
+//  Copyright © 2017年 尹艳玲. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
